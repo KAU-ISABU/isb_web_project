bbb9d946a7fd789f2f3d41abb38a7442243d433b
sj7699 <tkdwo7699@gmail.com>
2022-07-04 00:16:42 +0900

    test: servie예제 테스트 구현

---
+++ Backend/src/main/java/kau/isabu/web_project/service/MemberService.java
@@ -0,0 +1,36 @@
+package kau.isabu.web_project.service;
+
+import kau.isabu.web_project.domain.member;
+import kau.isabu.web_project.repository.MemorymemberRepository;
+import kau.isabu.web_project.repository.memberRepository;
+
+import java.lang.reflect.Member;
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+    private final memberRepository MemberRepository;
+
+    public MemberService(memberRepository memberRepository) {
+        MemberRepository = memberRepository;
+    }
+
+    public Long join(member Member){
+        validateDuplicateMember(Member);
+        MemberRepository.save(Member);
+        return Member.getId();
+    }
+
+    private void validateDuplicateMember(member Member) {
+        MemberRepository.findByName(Member.getName()).ifPresent(m->{
+            throw new IllegalStateException("이미 존재하는 회원입니다");
+        });
+    }
+    public List<member> findMembers(){
+        return MemberRepository.findAll();
+    }
+
+    public Optional<member> findOne(Long memberId){
+        return MemberRepository.findById(memberId);
+    }
+}
