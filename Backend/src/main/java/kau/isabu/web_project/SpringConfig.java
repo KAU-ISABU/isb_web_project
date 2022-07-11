d94220b3c5c7620283c74df7abf954d9829f4163
sj7699 <tkdwo7699@gmail.com>
2022-07-12 01:08:27 +0900

    test: jdbc postgresql 수정 (dataSource bean autoconfiguration작동안하는거 수정)

--- Backend/src/main/java/kau/isabu/web_project/SpringConfig.java
+++ Backend/src/main/java/kau/isabu/web_project/SpringConfig.java
@@ -1,25 +1,30 @@
 package kau.isabu.web_project;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import kau.isabu.web_project.repository.JdbcMemberReposity;
 import kau.isabu.web_project.repository.MemorymemberRepository;
 import kau.isabu.web_project.repository.memberRepository;
 import kau.isabu.web_project.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.datasource.DataSourceUtils;
 
 import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
-    private DataSource dataSource;
 
     @Autowired
     public SpringConfig(DataSource dataSource) {
         this.dataSource = dataSource;
     }
 
+    private DataSource dataSource;
     @Bean
     public MemberService memberService(){
         return new MemberService(MemberRepository());
